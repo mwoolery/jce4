8c89691f5762508a5f03231eda348f146df531a9
sandipSubedi <sandipsubedi@outlook.com>
2016-11-01 22:30:10 -0500

    sandip and matthew

--- controllers/estimatePartAggregate.js
+++ controllers/estimatePartAggregate.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var api = express.Router();
+var find =  require('lodash.remove');
+var findIndex = require('lodash.remove');
+var Model = require('../models/estimatePartAggregate.js');
+const notfoundstring = 'No such Estimate Part Aggregate';
 
 
 // see app.js for the root request this controller handles
+
+// see app.js to find  default URI for this controller (e.g., "waterproofingPrimer")
+// Specify the handler for each required combination of URI and HTTP verb 
+// HTML5 forms can only have GET and POST methods (use POST for DELETE)
+
+
+// HANDLE JSON REQUESTS --------------------------------------------
+api.get('/findall', function(req,res){
+  res.setHeader('Content-Type','application/json');
+  let data = req.app.locals.estimatePartAggregates.query;
+  res.send(JSON.stringify(data));
+});
+
 
 // GET to this controller root URI
 api.get("/", function (request, response) {
@@ -12,4 +29,8 @@ api.get("/", function (request, response) {
 
 
 
-module.exports = api;
+module.exports = api; // at the very end
+
+// This model is managed by Team R09
+// Sandip Subedi
+// Matthew Woolery
