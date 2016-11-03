adf46f76c56cbba8c22fd18a0876f4cf7c5defef
Edam <S528121@mail.nwmissouri.edu>
2016-11-02 22:25:37 -0500

    second commit

--- models/estimatePartFootage.js
+++ models/estimatePartFootage.js
@@ -11,3 +11,7 @@ var EstimatePartFootageSchema = new Schema({
 
 var estimatePartFootage = mongoose.model('EstimatePartFootage', EstimatePartFootageSchema)
 module.exports = estimatePartFootage
+// This model is managed by Team 4-4
+// Amarishwer Edam
+// Sivareddy Mekapothula
+// Vikas Bavikadi
