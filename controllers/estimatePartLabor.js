52a24fdb15791d0101da35b6c0b49ab6a3c74ac2
NWMSU\S528130 <S528130@mail.nwmissouri.edu>
2016-11-17 20:37:57 -0600

    commti

--- controllers/estimatePartLabor.js
+++ controllers/estimatePartLabor.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var api = express.Router();
-<<<<<<< HEAD
 var find = require('lodash.find');
 var remove = require('lodash.remove');
 var findIndex = require('lodash.findindex');
@@ -28,8 +27,6 @@ api.get('/findone/:id', function(req, res){
     var item = data.find(function(dt){ { return dt._id==req.params.id; }});
     res.send(JSON.stringify(item));
 });
-=======
->>>>>>> 4346a327c4ce3685a64cdf2ec315838906841661
 
 
 // see app.js for the root request this controller handles
@@ -40,7 +37,6 @@ api.get("/", function (request, response) {
 });
 
 
-<<<<<<< HEAD
 // GET create
 api.get("/create", function(req, res) {
     console.log('Handling GET /create' + req);
@@ -143,8 +139,6 @@ api.post('/delete/:id', function(req, res, next) {
     console.log("Deleted item " + JSON.stringify(item));
     return res.redirect('/estimatePartLabor');
 });
-=======
->>>>>>> 4346a327c4ce3685a64cdf2ec315838906841661
 
 
 module.exports = api;
