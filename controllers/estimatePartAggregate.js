be71361a0d553320bf2aa401debf0328d3027640
Matthew Woolery <S515755@mail.nwmissouri.edu>
2016-11-30 13:04:15 -0600

    stuff from in class

--- controllers/estimatePartAggregate.js
+++ controllers/estimatePartAggregate.js
@@ -15,14 +15,14 @@ const notfoundstring = 'No such Estimate Part Aggregate';
  */
 api.get('/findall', function(req, res){
     res.setHeader('Content-Type', 'application/json');
-    var data = req.app.locals.estimatePartAggregate.query;
+    var data = req.app.locals.estimatePartAggregates.query;
     res.send(JSON.stringify(data));
 });
 
 api.get('/findone/:id', function(req, res){
      res.setHeader('Content-Type', 'application/json');
     var id = parseInt(req.params.id);
-    var data = req.app.locals.estimatePartAggregate.query;
+    var data = req.app.locals.estimatePartAggregates.query;
     var item = find(data, { '_id': id });
     if (!item) { return res.end(notfoundstring); }
     res.send(JSON.stringify(item));
@@ -139,7 +139,7 @@ api.post('/save/:id', function(req, res) {
     console.log("Handling SAVE request" + req);
     var id = parseInt(req.params.id);
     console.log("Handling SAVING ID=" + id);
-    var data = req.app.locals.estimatePartAggregate.query;
+    var data = req.app.locals.estimatePartAggregates.query;
     var item = find(data, { '_id': id });
     if (!item) { return res.end(notfoundstring); }
     console.log("ORIGINAL VALUES " + JSON.stringify(item));
